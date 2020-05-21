c02c2e070f7ea1a9919bf0f2d5fd77a8815a5475
Zira Fox <aeseligman@gmail.com>
2020-05-21 00:12:47 -0400

    Factor render logic into renderBlip() function

--- source/main.c
+++ source/main.c
@@ -48,6 +48,32 @@ bool determineDirection(Direction* dir, u64 kDown)
     return true;
 }
 
+void renderBlip(Game* game, Blip* blip, u32* framebuf, u32 stride)
+{
+    double blip_cell_y = game->progress * (blip->target_row - blip->row) + blip->row;
+    double blip_cell_x = game->progress * (blip->target_column - blip->column) + blip->column;
+
+    blip_cell_y = blip->row;
+    blip_cell_x = blip->column;
+
+    u32 blip_y = (int)(FB_HEIGHT - blip_cell_y * CELL_SIZE);    // Flip vertically
+    u32 blip_x = (int)(blip_cell_x * CELL_SIZE);
+
+    // Each pixel is 4-bytes due to RGBA8888.
+    for (u32 y = blip_y - CELL_SIZE_HALF; y < blip_y + CELL_SIZE_HALF && y >= 0 && y < FB_HEIGHT; y ++)
+    {
+        for (u32 x = blip_x - CELL_SIZE_HALF; x < blip_x + CELL_SIZE_HALF && x >= 0 && x < FB_WIDTH; x ++)
+        {
+            u32 pos = y * stride / sizeof(u32) + x;
+#ifdef DISPLAY_IMAGE
+            framebuf[pos] = RGBA8_MAXALPHA(imageptr[pos*3+0]+(cnt*4), imageptr[pos*3+1], imageptr[pos*3+2]);
+#else
+            framebuf[pos] = 0xFFFFFFFF;//Set framebuf to white
+#endif
+        }
+    }
+}
+
 // TODO: Not implied that last_timestamp will update
 void getDeltaTime(float* out_delta_time, struct timeval* last_timestamp) 
 {
@@ -114,16 +140,6 @@ int main(int argc, char* argv[])
         u32 stride;
         u32* framebuf = (u32*) framebufferBegin(&fb, &stride);
 
-        Blip* blip = &game.root_blip;
-        double blip_cell_y = game.progress * (blip->target_row - blip->row) + blip->row;
-        double blip_cell_x = game.progress * (blip->target_column - blip->column) + blip->column;
-
-        blip_cell_y = blip->row;
-        blip_cell_x = blip->column;
-
-        u32 blip_y = (int)(FB_HEIGHT - blip_cell_y * CELL_SIZE);    // Flip vertically
-        u32 blip_x = (int)(blip_cell_x * CELL_SIZE);
-
         // Black out framebuffer TODO: Quick and dirty
         for (u32 y = 0; y < FB_HEIGHT; y ++)
         {
@@ -134,20 +150,9 @@ int main(int argc, char* argv[])
             }
         }
 
-        // Each pixel is 4-bytes due to RGBA8888.
-        for (u32 y = blip_y - CELL_SIZE_HALF; y < blip_y + CELL_SIZE_HALF && y >= 0 && y < FB_HEIGHT; y ++)
-        {
-            for (u32 x = blip_x - CELL_SIZE_HALF; x < blip_x + CELL_SIZE_HALF && x >= 0 && x < FB_WIDTH; x ++)
-            {
-                u32 pos = y * stride / sizeof(u32) + x;
-#ifdef DISPLAY_IMAGE
-                framebuf[pos] = RGBA8_MAXALPHA(imageptr[pos*3+0]+(cnt*4), imageptr[pos*3+1], imageptr[pos*3+2]);
-#else
-                framebuf[pos] = 0xFFFFFFFF;//Set framebuf to white
-#endif
-            }
-        }
-
+        renderBlip(&game, &game.root_blip, framebuf, stride);
+        // renderBlip(&game, &game.loose_blip, framebuf, stride);
+        
         // We're done rendering, so we end the frame here.
         framebufferEnd(&fb);
     }
